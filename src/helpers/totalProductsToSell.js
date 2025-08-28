41c8c36f47b0b255dbef9d9c0658b1ef1e7e1da5
auroraengelgeer <auroraengelgeer@hotmail.com>
2025-08-28 11:05:00 +0200

    shortened code in totalProductsToSell.js

--- src/helpers/totalProductsToSell.js
+++ src/helpers/totalProductsToSell.js
@@ -1,9 +1,17 @@
 import totalStockProduct from "./totalStockProduct.js";
 import totalNumberOfSoldProducts from "./totalNumberOfSoldProducts.js";
 
+//
+// function totalProductsToSell() {
+//     const productsToSell = totalStockProduct() - totalNumberOfSoldProducts();
+//     return productsToSell;
+// }
+//
+// export default totalProductsToSell;
+
 function totalProductsToSell() {
-    const productsToSell = totalStockProduct() - totalNumberOfSoldProducts();
-    return productsToSell;
+    return totalStockProduct() - totalNumberOfSoldProducts();
+
 }
 
 export default totalProductsToSell;
